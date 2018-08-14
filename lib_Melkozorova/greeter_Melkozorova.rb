e9d4f8de86ec1bb8bbe34bf2c23e67149c1952b8
Olha Melkozorova <Olha_Melkozorova@epam.com>
2018-08-14 09:21:08 +0300

    comit 1 in branch

---
+++ lib_Melkozorova/greeter_Melkozorova.rb
@@ -0,0 +1,14 @@
+class Greeter
+    def initialize(who)
+      @who=who 1
+@who=who 1
+@who=who 2
+    end
+<<<<<<< HEAD
+    def yreet 
+=======
+    def reet 
+>>>>>>> master
+     "Hello", #{@who}"
+    end
+1n
